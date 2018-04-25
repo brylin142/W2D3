39cee877bf071d53ea6f606394354fc3cec13073
Bryan Lin <33795878+brylin142@users.noreply.github.com>
2018-04-25 14:21:54 -0700

    Complete TDD exercises

--- TDD/lib/tdd.rb
+++ TDD/lib/tdd.rb
@@ -7,3 +7,60 @@ def my_uniq(arr)
   end
   non_duplicates
 end
+
+class Array
+  def two_sum
+    (0...length).to_a.combination(2).to_a.select { |x, y| self[x] + self[y] == 0 }
+  end
+end
+
+def my_transpose(matrix)
+  transpose = Array.new (matrix.length) { Array.new(matrix.length) }
+  matrix.each_index do |i|
+    matrix[i].each_index do |j|
+      transpose[j][i] = matrix[i][j]
+    end
+  end
+  transpose
+end
+
+def stock_picker(stocks)
+  current_largest = 0
+  result = []
+  i = 0
+  while i < stocks.length
+    j = i+1
+    while j < stocks.length
+      if  (stocks[j] - stocks[i] )> current_largest
+        result = [i,j]
+        current_largest = stocks[j] - stocks[i]
+      end
+      j +=1
+    end
+    i += 1
+  end
+  result
+end
+
+class TowersOfHanoi
+  attr_accessor :towers
+
+  def initialize
+    @towers = [[3, 2, 1], [], []]
+  end
+
+  def move(start_tower, end_tower)
+    raise "invalid move" if towers[start_tower].empty?
+    starting_disc, end_place = towers[start_tower].last, towers[end_tower].last
+    if end_place.nil? || starting_disc < end_place
+      towers[end_tower] << towers[start_tower].pop
+    else
+      raise "invalid move"
+    end
+    towers
+  end
+
+  def won?
+    towers[0].empty? && towers[1].length == 3 || towers[2].length == 3 ? true : false
+  end
+end
--- TDD/spec/tdd_spec.rb
+++ TDD/spec/tdd_spec.rb
@@ -9,7 +9,59 @@ describe "#my_uniq" do
 end
 
 describe "Array#two_sum" do
-    subject(:arr) {[-1,0,2,-2,1]}
-    it "returns the indexes of array elements that sum to 0"
+  subject(:arr) {[-1,0,2,-2,1]}
+  it "returns the indexes of array elements that sum to 0" do
     expect(arr.two_sum).to contain_exactly([0, 4], [2, 3])
+  end
+end
+
+describe "#my_transpose" do
+  subject(:matrix) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
+  it "takes in a matrix and returns the transposition" do
+    cols = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ]
+    expect(my_transpose(matrix)).to eq(cols)
+  end
+end
+
+describe "#stock_picker" do
+  subject(:stock_prices) {[12,43,61,14,6,0,35,1,8,9] }
+  it "takes in an array of stock prices and returns the most profitable pair of days" do
+    expect(stock_picker(stock_prices)).to eq([0,2])
+  end
+end
+
+describe TowersOfHanoi do
+  subject(:towers_game) {TowersOfHanoi.new}
+  describe "#move" do
+    it "throws an error if the user selects an empty array" do
+      expect{towers_game.move(1,0)}.to raise_error("invalid move")
+    end
+    it "doesn't allow user to move to tower with smaller disk" do
+      towers_game.move(0,1)
+      expect{towers_game.move(0,1)}.to raise_error("invalid move")
+    end
+    it "Correctly moves disk from one tower to another" do
+      towers_game.move(0,1)
+      expect(towers_game.towers[0]).to eq([3, 2])
+      expect(towers_game.towers[1]).to eq([1])
+    end
+  end
+  describe "#won?" do
+    it "returns false if disks aren't on same tower" do
+      towers_game.move(0,1)
+      expect(towers_game.won?).to be false
+    end
+    it 'returns false if disks have not moved to a different tower' do
+      expect(towers_game.won?).to be false
+    end
+    it 'return true if disks are on same tower that is not the original tower' do
+      towers_game.towers[0] = []
+      towers_game.towers[1] = [3,2,1]
+      expect(towers_game.won?).to be true
+    end
+  end
 end
